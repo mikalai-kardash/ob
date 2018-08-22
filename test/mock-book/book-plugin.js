0281cf9a263898ae28609a465ed5b29e04d48807
Mikalai Kardsh <mikalai.kardash@gmail.com>
2018-08-21 18:57:14 -0700

    Another.

--- test/mock-book/book-plugin.js
+++ test/mock-book/book-plugin.js
@@ -10,20 +10,6 @@ class BookPlugin {
     apply(compiler) {
         compiler.hooks.compilation.tap(PLUGIN, compilerCompilation());
         compiler.hooks.make.tapAsync(PLUGIN, compilerMake());
-
-        compiler.hooks.beforeCompile.tapAsync(PLUGIN, (compilation, cb) => {
-            console.log('compiler - beforeCompile');
-
-            return cb();
-        });
-
-        compiler.hooks.entryOption.tap(PLUGIN, () => {
-            console.log('compiler - entryOption');
-        });
-
-        compiler.hooks.shouldEmit.tap(PLUGIN, () => {
-            console.log('compiler - shouldEmit');
-        });
     }
 }
 
@@ -37,19 +23,6 @@ function compilerMake(options) {
 function compilerCompilation(options) {
     return function(compilation, { normalModuleFactory }) {
         console.log('compiler - compilation');
-
-        compilation.hooks.buildModule.tap(PLUGIN, () => {
-            console.log('compilation - buildModule');
-            
-        });
-
-        compilation.hooks.succeedModule.tap(PLUGIN, () => {
-            console.log('compilation - succeedModule ');
-        });
-
-        compilation.hooks.finishModules.tap(PLUGIN, () => {
-            console.log('compilation - finishModules ');
-        });
     };
 }
 
--- test/mock-book/trace-plugin.js
+++ test/mock-book/trace-plugin.js
@@ -1,17 +1,46 @@
 "use strict";
 
 const PLUGIN = 'TracePlugin';
+const chalk = require('chalk');
+const log = console.log;
+
+function header(target, event) {
+    const b = chalk.bgBlack;
+    const t = chalk.underline.dim;
+    const e = chalk.cyan.bold;
+    const message =
+        `
+    ${t(target.toLowerCase())} ${e(event)}
+`;
+
+    log(b(message));
+}
+
+class Writer {
+
+}
+
 
 class TracePlugin {
-    constructor() {
+    constructor({ verbose } = {}) {
+        this.verbose = verbose || false;
     }
 
     apply(compiler) {
-        compiler.hooks.beforeCompile.tapAsync(PLUGIN, function(params, cb) {
+        compiler.hooks.beforeCompile.tapAsync(PLUGIN, ({ compilationDependencies }, cb) => {
             // compilationDependencies
             // contextModuleFactory + hooks
             // normalModuleFactory + hooks
-            console.log(PLUGIN + ' beforeCompile');
+
+            header('compiler', 'beforeCompile');
+
+            const n = compilationDependencies.size;
+            const dependencies = n === 0
+                ? `${chalk.green.bold(n)} dependencies`
+                : `${chalk.yellow(n)} dependencies`;
+
+            log(dependencies);
+
             debugger;
             return cb();
         });
